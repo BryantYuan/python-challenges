1085ab887ae7868a8970508f6fbfd3836f82280a
Bryant Yuan <bryanty1130@gmail.com>
2024-05-18 15:06:42 +1200

    Do lamps2.py challenge

--- OUCC/2023/round2/lamps2.py
+++ OUCC/2023/round2/lamps2.py
@@ -2,8 +2,8 @@ result: list = []
 start: list = [['0' for i in range(5)] for j in range(5)]
 
 for i in range(5):
-    row = input().split(' ')
-    result.append(row)
+    row = input()
+    result.append([i for i in row])
 possibles: dict = {}
 
 def change(grid: list, _pos: int, direction: str) -> list:
@@ -28,20 +28,23 @@ def iterate(_row: int, _col: int, _start: list) -> int :
     total = 0
     if _row == 4 and _col == 4:
         return 0
+    print(_row, _col)
     if start[_row][_col] != result[_row][_col]:
         if (_row, _col) in possibles.keys():
             pos = possibles[_row][_col]
         else:
             pos = ['R', 'C']
-
+        if _col + 1 >= 5:
+            _col = 0
+            _row += 1
+        else:
+            _col += 1
         for p in pos:
-            if _row + 1 == 5:
-                _col += 1
-                _row = 0
-            else:
-                _row += 1
             total += 1
-            new = iterate(_row, _col, change(_start, _row, p))
+            if p == 'C':
+                new = iterate(_row, _col, change(_start, _col, p))
+            else:
+                new = iterate(_row, _col, change(_start, _row, p))
             total += new
             if total < best:
                 best = total

a486d5bd6ad4cc062db4b9c4b77fbd09eebd9854
Bryant Yuan <bryanty1130@gmail.com>
2024-05-19 17:59:39 +1200

    Try finish lamps2.py challenge

--- OUCC/2023/round2/lamps2.py
+++ OUCC/2023/round2/lamps2.py
@@ -9,65 +9,43 @@ possibles: dict = {}
 
 
 def change(grid: list, _row: int, _col: int) -> list:
-    if grid[_row][_col] == 0:
-        grid[_row][_col] = 1
+    if grid[_row][_col] == '0':
+        grid[_row][_col] = '1'
     else:
-        grid[_row][_col] = 0
+        grid[_row][_col] = '0'
     return grid
 
 
-def change_dict(dictionary: dict, _row: int, _col: int, _pos) -> dict:
-    dictionary[(_row, _col)] = _pos
-    return dictionary
+total = 0
+for _row in range(5):
+    for _col in range(5):
+        # print(_row, _col)
 
+        if start[_row][_col] != result[_row][_col]:
+            # print('OK?')
+            total += 1
 
-def next_index(_row: int, _col: int) -> (int, int):
-    if _col + 1 >= 5:
-        _col = 0
-        _row += 1
-    else:
-        _col += 1
-    return _row, _col
+            if (_row, _col) in possibles:
+                pos = possibles[(_row, _col)]
+            else:
+                pos = ['C']
+                for i in range(5):
+                    possibles[(_row, i)] = ['C']
+                for i in range(5):
+                    possibles[(i, _col)] = ['R']
 
+            for p in pos:
+                for i in range(5):
 
-def iterate(_row: int, _col: int, start: list) -> int:
-    best = float('inf')
-    total = 0
-    print(_row, _col)
+                    if p == 'R':
+                        change(start, _row, i)
+                    else:
+                        change(start, i, _col)
 
-    if _row == 4 and _col == 4:
-        return 0
-
-    if start[_row][_col] != result[_row][_col]:
-
-        if (_row, _col) in possibles:
-            pos = possibles[(_row, _col)]
+        if _col + 1 >= 5:
+            _col = 0
+            _row += 1
         else:
-            pos = ['R', 'C']
-            for i in range(5):
-                change_dict(possibles, _row, i, ['C'])
-            for i in range(5):
-                change_dict(possibles, i, _row, ['R'])
+            _col += 1
 
-        index = next_index(_row, _col)
-        _row = index[0]
-        _col = index[1]
-        total += 1
-
-        for p in pos:
-            for i in range(5):
-
-                if p == 'R':
-                    start = change(start, _row, i)
-                else:
-                    start = change(start, i, _col)
-
-            new = iterate(_row, _col, start)
-            total += new
-            if total < best:
-                best = total
-
-    return best
-
-
-print(iterate(0, 0, start))
+print(total)
--- test.py
+++ test.py
@@ -94,6 +94,16 @@
 # else:
 #     print(-1)
 
-global hi
-hi = 0
-print(hi)
+grid = {(i,j):False for i in range(1,6) for j in range(1,6)}
+commands = [(int(x[0]),x[1]) for x in input().split(' ')]
+
+for command in commands:
+  if command[1] == 'R':
+    lamps = [(command[0],j) for j in range(1,6)]
+  else:
+    lamps = [(i,command[0]) for i in range(1,6)]
+  for lamp in lamps:
+    grid[lamp] = not grid[lamp]
+
+for i in range(1,6):
+  print(''.join(str(int(grid[(i,j)])) for j in range(1,6)))

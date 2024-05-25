6c2c8dcfc655a4a9363ec61bc08b91fec1261920
Bryant Yuan <bryanty1130@gmail.com>
2024-05-25 15:38:07 +1200

    Try finish lamps2.py challenge

--- OUCC/2023/round2/lamps2.py
+++ OUCC/2023/round2/lamps2.py
@@ -1,11 +1,17 @@
 result: list = []
-start: list = [['0' for i in range(5)] for j in range(5)]
 
 for i in range(5):
     row = input()
     result.append([i for i in row])
 
-possibles: dict = {}
+
+
+
+def opposite(direction):
+    if direction == 'R':
+        return 'C'
+    else:
+        return 'R'
 
 
 def change(grid: list, _row: int, _col: int) -> list:
@@ -16,36 +22,40 @@ def change(grid: list, _row: int, _col: int) -> list:
     return grid
 
 
-total = 0
-for _row in range(5):
-    for _col in range(5):
-        # print(_row, _col)
+best = float('inf')
+for _ in ['R', 'C']:
+    possibles: dict = {}
+    total = 0
+    possibles[(0,0)] = _
+    start: list = [['0' for i in range(5)] for j in range(5)]
 
-        if start[_row][_col] != result[_row][_col]:
-            # print('OK?')
-            total += 1
+    for _row in range(5):
 
-            if (_row, _col) in possibles:
-                pos = possibles[(_row, _col)]
-            else:
-                pos = ['C']
+        for _col in range(5):
+
+            if start[_row][_col] != result[_row][_col]:
+                total += 1
+
+                if (_row, _col) in possibles:
+                    pos = possibles[(_row, _col)][0]
+
+                    for i in range(5):
+                        if (_row, i) not in possibles:
+                            possibles[(_row, i)] = [opposite(pos)]
+
+                    for i in range(5):
+                        if (i, _col) not in possibles:
+                            possibles[(i, _col)] = [pos]
+                else:
+                    pos = ['R', 'C']
+
                 for i in range(5):
-                    possibles[(_row, i)] = ['C']
-                for i in range(5):
-                    possibles[(i, _col)] = ['R']
 
-            for p in pos:
-                for i in range(5):
-
-                    if p == 'R':
+                    if pos == 'R':
                         change(start, _row, i)
                     else:
                         change(start, i, _col)
+    if total < best:
+        best = total
 
-        if _col + 1 >= 5:
-            _col = 0
-            _row += 1
-        else:
-            _col += 1
-
-print(total)
+print(best)

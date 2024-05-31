0c142a76008f80aabfcf0d08060c99ac15a14759
david <david.yuan124@gmail.com>
2024-06-01 10:22:28 +1200

    update

--- OUCC/2023/round2/lamps2.py
+++ OUCC/2023/round2/lamps2.py
@@ -6,42 +6,32 @@ for i in range(5):
 
 _len = len(result)
 
+
 # find start node and initialize nodeAndDirection
 def compare_and_assign_to_dict(_row, _col, _dir):
     if cur_board[_row][_col] != result[_row][_col]:
         nodeAndDirection[(_row, _col)] = _dir if _dir == 'R' else 'C'
 
 
-def change(grid: list, _row: int, _col: int) -> list:
-    if grid[_row][_col] == '0':
-        grid[_row][_col] = '1'
-    else:
-        grid[_row][_col] = '0'
-    return grid
-
-
-
-
-# print(nodeAndDirection)
 start_node = (0, 0)
 best = float('inf')
+
+for _row in range(_len):
+    for _col in range(_len):
+        if result[_row][_col] == '1':
+            cur_col = _col + 1
+            for cur_col in range(cur_col, _len):
+                compare_and_assign_to_dict(_row, cur_col, 'R')
+            cur_row = _row + 1
+            for cur_row in range(cur_row, _len):
+                compare_and_assign_to_dict(cur_row, _col, 'C')
+            break
 
 cur_row = start_node[0]
 for _dir in ['R', 'C']:
     total = 0
     cur_board: list = [['0' for i in range(_len)] for j in range(_len)]
-    nodeAndDirection: dict = {(0,0): _dir}
-
-    for _row in range(_len):
-        for _col in range(_len):
-            if result[_row][_col] == '1':
-                cur_col = _col + 1
-                for cur_col in range(cur_col, _len):
-                    compare_and_assign_to_dict(_row, cur_col, 'R')
-                cur_row = _row + 1
-                for cur_row in range(cur_row, _len):
-                    compare_and_assign_to_dict(cur_row, _col, 'C')
-                break
+    nodeAndDirection: dict = {(0, 0): _dir}
 
     for _row in range(start_node[0], _len):
         for _col in range(start_node[1], _len):
@@ -49,20 +39,17 @@ for _dir in ['R', 'C']:
             if cur_board[_row][_col] != result[_row][_col]:
                 total += 1
 
+                direction = nodeAndDirection[(_row, _col)]
 
-                if (_row, _col) in nodeAndDirection:
-                    direction = nodeAndDirection[(_row, _col)]
-
-                for i in range(5):
-
+                for i in range(_len):
                     if direction == 'R':
-                        change(cur_board, _row, i)
+                        cur_board[_row][i] = '1' if cur_board[_row][i] == '0' else '0'
                     else:
-                        change(cur_board, i, _col)
+                        cur_board[i][_col] = '1' if cur_board[i][_col] == '0' else '0'
 
-            for j in range(1, 5):
+            for j in range(1, _len):
                 compare_and_assign_to_dict(_row, j, 'C')
-            for j in range(1, 5):
+            for j in range(1, _len):
                 compare_and_assign_to_dict(j, _col, 'R')
 
     if total < best:

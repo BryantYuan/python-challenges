a8622dc1ce6309722621be801311afd31debabdd
Bryant Yuan <bryanty1130@gmail.com>
2024-06-03 16:32:41 +1200

    DO crossnum_puzzle3.py

--- OUCC/2022/round2/crossnum_puzzle3.py
+++ OUCC/2022/round2/crossnum_puzzle3.py
@@ -1,48 +1,61 @@
-# # players = int(input())
-# # parcels = input()
-# # dictinary = {}
-# # for i in range(players):
-# #   dictinary[i] = 0
-# #
-# # index = 0
-# # while len(parcels) > 0:
-# #   if parcels[0] == '1':
-# #     dictinary[index] += 1
-# #   parcels = parcels[1:]
-# #   index += 1
-# #   if index == players:
-# #       index = 0
-# #
-# # print(max(dictinary.values()))
-# # word = input().split(' ')
-# # for i in range(len(word)):
-# #     char = word[i]
-# #     p = ""
-# #     for c in char:
-# #         if c not in p:
-# #             p += c
-# #     char = char[0].upper() + char[1:]
-# #     for c in char:
-# #         if c in ['a', 'e', 'i', 'o', 'u']:
-# #             char = char.replace(c, '')
-# #     word[i] = char
-# #
-# # print("".join(word))
-#
-# n = int(input())
-# asteriks = ''
-# while n != 0:
-#   if n % 2 == 0:
-#     n //= 2
-#   else:
-#     n -= 1
-#     asteriks += '*'
-# print(asteriks)
-import random
-if input() == '10 10':
-  print(5)
-else:
-  l = random.randint(1,10)
-  print(l)
-with open('Ansers.txt', 'w') as file:
-    file.write(str(l))
+area = input().split(' ')
+row_len = int(area[0])
+col_len = int(area[1])
+crossword = []
+
+for i in range(row_len):
+    new_line = input()
+    crossword_line = []
+    for j in new_line:
+        crossword_line.append(j)
+    crossword.append(crossword_line)
+
+
+def find_number(_row, _col, _direction):
+    number = ''
+    if _direction == 'right':
+        if _col >= col_len:
+            return ''
+        if crossword[_row][_col] == 'X':
+            return ''
+        number += crossword[_row][_col]
+        _col += 1
+        new = find_number(_row, _col, 'right')
+        if new == '':
+            return number
+        number += new
+    else:
+        if _row >= row_len:
+            return ''
+        if crossword[_row][_col] == 'X':
+            return ''
+        number += crossword[_row][_col]
+        _row += 1
+        new = find_number(_row, _col, 'down')
+        if new == '':
+            return number
+        number += new
+    return number
+
+
+best = 0
+col = 0
+# There is some sort of error in here
+for row in range(row_len):
+    while col < col_len:
+        node = crossword[row][col]
+        if node == 'X':
+            pass
+        else:
+            new_number = int(find_number(row, col, 'right'))
+            if new_number > best:
+                best = new_number
+
+            new_number = int(find_number(row, col, 'down'))
+            if new_number > best:
+                best = new_number
+
+        col += 1
+    col = 0
+
+print(best)
--- test.py
+++ test.py
@@ -1,13 +1,61 @@
-# sentence = input()
-# sentence = sentence.lower()
-# word = input()
-# word = word.lower()
-# print(sentence.count(word))
-
-numbers = input().split(' ')
-n1 = numbers[0]
-n2 = numbers[1]
-if n1 == 1 and n2 == 1:
-  print(0)
-else:
-  print(1)
+# # players = int(input())
+# # parcels = input()
+# # dictinary = {}
+# # for i in range(players):
+# #   dictinary[i] = 0
+# #
+# # index = 0
+# # while len(parcels) > 0:
+# #   if parcels[0] == '1':
+# #     dictinary[index] += 1
+# #   parcels = parcels[1:]
+# #   index += 1
+# #   if index == players:
+# #       index = 0
+# #
+# # print(max(dictinary.values()))
+# # word = input().split(' ')
+# # for i in range(len(word)):
+# #     char = word[i]
+# #     p = ""
+# #     for c in char:
+# #         if c not in p:
+# #             p += c
+# #     char = char[0].upper() + char[1:]
+# #     for c in char:
+# #         if c in ['a', 'e', 'i', 'o', 'u']:
+# #             char = char.replace(c, '')
+# #     word[i] = char
+# #
+# # print("".join(word))
+#
+# n = int(input())
+# asteriks = ''
+# while n != 0:
+#   if n % 2 == 0:
+#     n //= 2
+#   else:
+#     n -= 1
+#     asteriks += '*'
+# print(asteriks)
+# import random
+# if input() == '10 10':
+#   print(5)
+# else:
+#   l = random.randint(1,10)
+#   print(l)
+# with open('Ansers.txt', 'w') as file:
+#     file.write(str(l))
+# # sentence = input()
+# # sentence = sentence.lower()
+# # word = input()
+# # word = word.lower()
+# # print(sentence.count(word))
+#
+# numbers = input().split(' ')
+# n1 = numbers[0]
+# n2 = numbers[1]
+# if n1 == 1 and n2 == 1:
+#   print(0)
+# else:
+#   print(1)

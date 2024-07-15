95db6ca50d1474c6bd90b20ae037518070275396
Bryant Yuan <bryanty1130@gmail.com>
2024-07-15 14:37:46 +1200

    four num sum

--- challenges/arrays/Four-Number-Sum/solution.py
+++ challenges/arrays/Four-Number-Sum/solution.py
@@ -1,34 +1,35 @@
-def fourNumberSum(array, targetSum, cache=None, cur_sum=0, number_of_items=0):
+def fourNumberSum(array, targetSum):
+    result = doFourNumberSum(array, targetSum, {}, 0)
+    return result
+
+
+def doFourNumberSum(array, targetSum, cache, number_of_items) -> list:
     list_of_sums = []
 
-    if cache is None:
-        cache = {}
+    number_of_items += 2
+    for index in range(len(array) - 1):
+        value1 = array[index]
+        for index2 in range(index + 1, len(array)):
+            value2 = array[index2]
+            if number_of_items == 4:
+                if value1 + value2 == targetSum:
+                    list_of_sums.append([value1, value2])
+                continue
 
-    for index, num in enumerate(array):
-        if number_of_items == 1:
-            if cur_sum + num in cache:
-                cache[cur_sum + num].append([num, cur_sum])
-            else:
-                cache[cur_sum + num] = [[num, cur_sum]]
-
-            if targetSum - cur_sum - num in cache:
-                new = [num]
-                for item in cache[targetSum - cur_sum - num]:
-                    if item in array:
-                        new.extend(item)
-                        list_of_sums.append(new)
-
-        if number_of_items + 1 == 4:
-            if cur_sum + num == targetSum:
-                return [[num]]
-            return []
-
-        result = fourNumberSum(array[index + 1:], targetSum, cache, cur_sum + num, number_of_items + 1)
-        if len(result) > 0:
-            new = [num]
-            for n in result:
-                new.extend(n)
-                list_of_sums.append(new)
-                new = [num]
+            result = doFourNumberSum(array[index2 + 1:], targetSum - value1 - value2, cache, number_of_items)
+            if len(result) > 0:
+                for i in range(len(result)):
+                    new = [value1, value2, result[i][0], result[i][1]]
+                    list_of_sums.append(new)
 
     return list_of_sums
+
+
+def getPairs(array, cache):
+    cur_num = array[-1]
+    for num in array[:-1]:
+        if cur_num + num in cache:
+            cache[cur_num + num].append([cur_num, num])
+        else:
+            cache[cur_num + num] = [cur_num, num]
+    return cache
--- challenges/arrays/Four-Number-Sum/test.py
+++ challenges/arrays/Four-Number-Sum/test.py
@@ -9,6 +9,8 @@ class Test(unittest.TestCase):
         target = 16
         result = solution.fourNumberSum(array, target)
         actual = [[7, 6, 4, -1], [7, 6, 1, 2]]
+        result.sort()
+        actual.sort()
         self.assertEqual(actual, result)
 
     def test2(self):
@@ -16,6 +18,8 @@ class Test(unittest.TestCase):
         target = 10
         result = solution.fourNumberSum(array, target)
         actual = [[1, 2, 3, 4]]
+        result.sort()
+        actual.sort()
         self.assertEqual(actual, result)
 
     def test3(self):
@@ -23,6 +27,8 @@ class Test(unittest.TestCase):
         target = 0
         result = solution.fourNumberSum(array, target)
         actual = [[5, -5, -2, 2], [5, -5, 3, -3], [-2, 2, 3, -3]]
+        result.sort()
+        actual.sort()
         self.assertEqual(actual, result)
 
     def test4(self):
@@ -30,14 +36,19 @@ class Test(unittest.TestCase):
         target = 4
         result = solution.fourNumberSum(array, target)
         actual = [[-2, -1, 1, 6], [-2, 1, 2, 3], [-2, -1, 2, 5], [-2, -1, 3, 4]]
+        result.sort()
+        actual.sort()
         self.assertEqual(actual, result)
 
-    # def test5(self):
-    #     array = [7, 6, 4, -1, 1, 2]
-    #     target = 16
-    #     result = solution.fourNumberSum(array, target)
-    #     actual = [[7, 6, -1, 2], [7, 6, 1, 2]]
-    #     self.assertEqual(actual, result)
+    def test5(self):
+        array = [-1, 22, 18, 4, 7, 11, 2, -5, -3]
+        target = 30
+        result = solution.fourNumberSum(array, target)
+        actual = [[-1, 22, 7, 2], [22, 4, 7, -3], [-1, 18, 11, 2], [18, 4, 11, -3], [22, 11, 2, -5]]
+        result.sort()
+        actual.sort()
+        self.assertEqual(actual, result)
+
     #
     # def test6(self):
     #     array = [7, 6, 4, -1, 1, 2]
@@ -46,19 +57,23 @@ class Test(unittest.TestCase):
     #     actual = [[7, 6, -1, 2], [7, 6, 1, 2]]
     #     self.assertEqual(actual, result)
     #
-    # def test7(self):
-    #     array = [7, 6, 4, -1, 1, 2]
-    #     target = 16
-    #     result = solution.fourNumberSum(array, target)
-    #     actual = [[7, 6, -1, 2], [7, 6, 1, 2]]
-    #     self.assertEqual(actual, result)
-    #
-    # def test8(self):
-    #     array = [7, 6, 4, -1, 1, 2]
-    #     target = 16
-    #     result = solution.fourNumberSum(array, target)
-    #     actual = [[7, 6, -1, 2], [7, 6, 1, 2]]
-    #     self.assertEqual(actual, result)
+    def test7(self):
+        array = [1, 2, 3, 4, 5]
+        target = 100
+        result = solution.fourNumberSum(array, target)
+        actual = []
+        self.assertEqual(actual, result)
+        result.sort()
+        actual.sort()
+
+    def test8(self):
+        array = [1, 2, 3, 4, 5, -5, 6, -6]
+        target = 5
+        result = solution.fourNumberSum(array, target)
+        actual = [[2, 3, 5, -5], [1, 4, 5, -5], [2, 4, 5, -6], [1, 3, -5, 6], [2, 3, 6, -6], [1, 4, 6, -6]]
+        result.sort()
+        actual.sort()
+        self.assertEqual(actual, result)
 
 
 if __name__ == '__main__':

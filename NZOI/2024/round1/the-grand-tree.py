d6552676fe4835cde9d30425b1087d65f98cd8cb
Bryant Yuan <bryanty1130@gmail.com>
2024-09-14 15:18:41 +1200

    do a problem

---
+++ NZOI/2024/round1/the-grand-tree.py
@@ -0,0 +1,59 @@
+"""Time has come to pick the apples from The Grand Tree. The Grand Tree has N apples
+numbered 0 to N−1, and N−1 branches. Each branch connects two different apples. Any
+two apples are connected by exactly one sequence of branches. Each apple has a
+tastiness level that you, as a skilled apple picker, can determine just by looking at
+it. Unfortunately, The Grand Tree is rather Grand™, which means that you can only pick
+an apple by climbing up to it. Even more unfortunately, you are a bit clumsy, and
+cannot climb past an apple without knocking it off and damaging it. As such, you can
+climb along the branches to reach any apples you want, but you must pick every apple
+you climb past on the way. The trunk of the tree is located at apple 0, meaning you
+always start there, and you must pick apple 0 in order to begin climbing the tree.
+You may always pick apple 0.
+
+You have decided ahead of time that you wish to harvest a selection of apples where
+the sum of the tastiness of all the apples you have picked is equal to M.
+You however, are aware that that might not always be possible, so you have decided
+to settle for the smallest possible sum of tastiness greater than or equal to M.
+Due to the staggering size of the tree, you cannot work out which apples to pick in
+your head, and thus have decided to sit down and write a program instead."""
+
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.next = []
+
+
+applesAmount, tastinessWanted = tuple(map(int, input().split(' ')))  # Turns it into an int
+allApples = input().split(' ')
+
+root = BinaryTree(int(allApples[0]))
+allNodes = [root]
+for apple in allApples[1:]:
+    allNodes.append(BinaryTree(int(apple)))
+
+for i in range(applesAmount - 1):
+    branch1, branch2 = tuple(map(int, input().split(' ')))
+    allNodes[branch1].next.append(allNodes[branch2])
+
+
+def pickApples(_root: BinaryTree, currentSum):
+    closest_taste = -1
+    diff = float('inf')
+    newSum = currentSum + _root.value
+    if newSum >= tastinessWanted:
+        return newSum
+
+    nextNodes = _root.next
+
+    for nextVal in nextNodes:
+        applesCollected = pickApples(nextVal, newSum)
+        if applesCollected >= tastinessWanted:
+            if applesCollected - tastinessWanted < diff:
+                diff = applesCollected - tastinessWanted
+                closest_taste = applesCollected
+
+    return closest_taste
+
+
+print(pickApples(root, 0))

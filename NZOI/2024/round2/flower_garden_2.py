30b822334e78e1c0ea31d6f6055972ffd2d03949
Bryant Yuan <bryanty1130@gmail.com>
2024-10-27 19:23:47 +1300

    do flower garden 2. MAke into function

--- NZOI/2024/round2/flower_garden_2.py
+++ NZOI/2024/round2/flower_garden_2.py
@@ -1,29 +1,17 @@
-times, distance = map(int, input().split(' '))
+input_amount, distance = map(int, input().split(' '))
 total_width = distance * 2 + 1
-list_of_areas = []
-longest_height = -1
-longest_width = -1
+longest_y = -1
+longest_x = -1
 total = 0
+list_of_nodes = []
 
-for i in range(times):
-    center = input().split(' ')
-    x = int(center[0])
-    y = int(center[1])
 
-    if y + distance > longest_height:
-        for j in range(y - longest_height + distance):
-            list_of_areas.append(['0'] * (longest_width + 1))
-        longest_height = y + distance
-
-    if x + distance > longest_width:
-        for j in range(longest_height + 1):
-            list_of_areas[j].extend(['0'] * (x - longest_width + 1 + distance))
-        longest_width = x + distance
-
-    h_start = y - distance
-    h_end = y + distance
-    w_start = x - distance
-    w_end = x + distance
+def fill(_board, _x, _y):
+    h_start = _y - distance
+    h_end = _y + distance
+    w_start = _x - distance
+    w_end = _x + distance
+    cur = 0
 
     for height in range(h_start, h_end + 1):
         if height < 0:
@@ -33,9 +21,29 @@ for i in range(times):
             if width < 0:
                 continue
 
-            node = list_of_areas[height][width]
+            node = board[height][width]
             if node == '0':
-                list_of_areas[height][width] = '1'
-                total += 1
+                board[height][width] = '1'
+                cur += 1
+    return cur
+
+
+for i in range(input_amount):
+    center = input().split(' ')
+    x = int(center[0])
+    y = int(center[1])
+    list_of_nodes.append((x, y))
+
+    if y + distance + 1 > longest_y:
+        longest_y = y + distance + 1
+
+    if x + distance + 1 > longest_x:
+        longest_x = x + distance + 1
+
+board = [['0' for i in range(longest_x)] for i in range(longest_y)]
+
+for pair in list_of_nodes:
+    x, y = pair
+    total += fill(board, x, y)
 
 print(total)
